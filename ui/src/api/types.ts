1a38e9b1b8bbd6e986ea195b348a1e8f6906adcf
Alex Wendland <a.p.wendland@gmail.com>
2026-09-05 23:06:02 +0100

    feat: Add openapi spec for game actions (#10)

--- ui/src/api/types.ts
+++ ui/src/api/types.ts
@@ -42,6 +42,74 @@ export interface paths {
         patch?: never;
         trace?: never;
     };
+    "/api/game/tictactoe": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** Create tictactoe game */
+        post: operations["createGame"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/game/{game_id}/metadata": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Gets the metadata of a given game id */
+        get: operations["getGameMetadata"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/game/{game_id}/stream": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        /** Subscribe to the game event stream */
+        get: operations["getGameStream"];
+        put?: never;
+        post?: never;
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
+    "/api/game/{game_id}/action": {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        get?: never;
+        put?: never;
+        /** Make an action within the game */
+        post: operations["makeAction"];
+        delete?: never;
+        options?: never;
+        head?: never;
+        patch?: never;
+        trace?: never;
+    };
 }
 export type webhooks = Record<string, never>;
 export interface components {
@@ -65,6 +133,22 @@ export interface components {
         };
         Error: {
             message?: string;
+        };
+        GameCreatedResponse: {
+            game_id: string;
+        };
+        GameMetadataResponse: {
+            game_type: string;
+            metadata?: {
+                [key: string]: unknown;
+            };
+        };
+        GameActionRequest: {
+            /** @description Command type */
+            type: string;
+            payload: {
+                [key: string]: unknown;
+            };
         };
     };
     responses: {
@@ -93,6 +177,15 @@ export interface components {
             };
             content: {
                 "application/json": components["schemas"]["Error"];
+            };
+        };
+        /** @description Game created */
+        CreatedGame: {
+            headers: {
+                [name: string]: unknown;
+            };
+            content: {
+                "application/json": components["schemas"]["GameCreatedResponse"];
             };
         };
     };
@@ -249,6 +342,100 @@ export interface operations {
                     "application/json": components["schemas"]["UserResponse"];
                 };
             };
+        };
+    };
+    createGame: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path?: never;
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            201: components["responses"]["CreatedGame"];
+            401: components["responses"]["Unauthorised"];
+        };
+    };
+    getGameMetadata: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                game_id: string;
+            };
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Game metadata */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "application/json": components["schemas"]["GameMetadataResponse"];
+                };
+            };
+            401: components["responses"]["Unauthorised"];
+            404: components["responses"]["NotFound"];
+        };
+    };
+    getGameStream: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                game_id: string;
+            };
+            cookie?: never;
+        };
+        requestBody?: never;
+        responses: {
+            /** @description Game event stream */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content: {
+                    "text/event-stream": string;
+                };
+            };
+            401: components["responses"]["Unauthorised"];
+            404: components["responses"]["NotFound"];
+        };
+    };
+    makeAction: {
+        parameters: {
+            query?: never;
+            header?: never;
+            path: {
+                game_id: string;
+            };
+            cookie?: never;
+        };
+        requestBody: {
+            content: {
+                "application/json": components["schemas"]["GameActionRequest"];
+            };
+        };
+        responses: {
+            /** @description Request to make action placed */
+            200: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            /** @description Invalid move */
+            400: {
+                headers: {
+                    [name: string]: unknown;
+                };
+                content?: never;
+            };
+            401: components["responses"]["Unauthorised"];
+            404: components["responses"]["NotFound"];
         };
     };
 }
